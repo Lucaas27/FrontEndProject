a2bdcc94691c9800fac0a7d532f516c2efd8927d
Lucas Gomes <lucasggomees@gmail.com>
2020-04-07 01:55:07 +0100

    Added new pictures in order to create a grid system based gallery

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 const toggleButton = document.getElementById("toggle-button");
 const navbarLinks = document.querySelector("nav ul.navbar-links");
 
-// listens fot clicks on the hamburguer menu
+// listens fot clicks on the hamburguer menu and adds a class called active
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
